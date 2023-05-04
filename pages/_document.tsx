7da5258dfdf0e8c67060bbf7d454dcdd94f00956
kj1010g <kjkagit@gmail.com>
2023-05-04 13:22:10 +0530

    testing push, changed title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
 					name="viewport"
 					content="width=device-width, initial-scale=1"
 				></meta>
-				<title>Atri Labs</title>
+				<title>Atri TEST Labs</title>
 				<AtriScripts />
 			</head>
 			<body>
